9b6719e671e11bdc48a16d765da8fb5ed6e2cecd
Jovanay Carter <69544825+jovanayc@users.noreply.github.com>
2023-02-15 14:00:19 -0500

    practice and hw6 q1

---
+++ tandon_HW6/N11316951_hw6_q1.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+int fib(int n);//initialize function
+
+int main(){//call main function
+    //set variables
+    double num; 
+    int nFib;
+
+    //take user input
+    cout<<"Please enter a positive integer: ";
+    cin>>num;
+
+    //sent user input through fib function
+    nFib = fib(num);
+    cout<<"Fibonacci sequence is "<<nFib<<endl;
+
+    //end function
+    return 0;
+}
+
+int fib(int n){ //defining the function
+    int solution, i, oneBefore, twoBefore;
+    
+    solution = 0;
+    oneBefore = 0;
+    twoBefore = 0; //fn-1 + fn-2 = fn
+
+    for (i=0; i<n; i++){
+        if (i==0){
+        solution = i+1;
+        oneBefore = solution;
+        cout<<"Solution: "<<solution<<endl;
+        } else {   
+            solution = oneBefore+twoBefore;
+            twoBefore = oneBefore;
+            oneBefore = solution;
+            cout<<"Solution: "<<solution<<endl;
+            }
+    } 
+    return solution;
+}
