bdac5c412ba993339bbce895da7cd10249155809
Jovanay Carter <69544825+jovanayc@users.noreply.github.com>
2023-03-08 16:00:15 -0500

    hw8q1

---
+++ tandon_HW8/N11316951_hw8_q1.cpp
@@ -0,0 +1,52 @@
+//Jovanay Carter, N11316951 HW8 Q1, Winter Extended Bridge
+//[NOTSUBMITTED] done on March 8, 2023
+
+/*Prompt: Write a program with an array that takes in user values
+  and prints the minimum value in the array */
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int minInArray (int arr[], int arrrSize);
+
+int main() {
+    int arrSize = 20;
+    int inputArr[arrSize];
+    int ind, currentNum;
+
+    cout << "Please enter values for your array: " << endl;
+    
+    //Add user values into array "arr"
+    for (ind = 0; ind < arrSize; ind++){
+        cin >> currentNum;
+        inputArr[ind] = currentNum;
+    }
+
+    minInArray (inputArr, arrSize);
+
+    return 0;
+}
+
+int minInArray (int arr[], int arrSize){
+    //int arr[arrSize];
+    int ind, min, currentNum;
+    //int appearances [];
+
+    //Look through array indecies for minimum values
+    for (ind = 0; ind < arrSize; ind++){
+        if (arr[ind] < arr[ind-1])
+            min = arr[ind];
+    }
+
+    //return final Minimum
+    cout << "The mimum value is " << min << ", and it is located in the following indicies: ";
+
+    //cout all indecies that contain the minimum value
+    for (ind = 0; ind < arrSize; ind++){
+        if (min == arr[ind])
+            cout << ind << " ";
+    }
+
+    return min;
+}
