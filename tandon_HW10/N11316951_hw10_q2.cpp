35330bf8b27e2bd28712f8533688a0236895ce42
Jovanay Carter <69544825+jovanayc@users.noreply.github.com>
2023-03-16 16:26:34 -0400

    Hw10 q2

--- tandon_HW10/N11316951_hw10_q2.cpp
+++ tandon_HW10/N11316951_hw10_q2.cpp
@@ -8,4 +8,86 @@ Note: Main should test this function.
 */
 
 #include <iostream>
-#
+#include <string>
+using namespace std;
+
+int* findMissing (int arr[], int n, int& resArrSize);
+
+int main() {
+    //test array input
+    int arr [6] = {2, 2, 2, 2, 4, 6};
+    int n = 6;
+    int resArrSize = 0;
+    findMissing (arr, n, resArrSize);
+    return 0;
+}
+
+int* findMissing (int arr[], int n, int& resArrSize) {
+    bool numIsPresent;
+    resArrSize = n;
+    int* comparisonArr = new int [resArrSize];
+    int* missingNumsArr = new int [resArrSize];
+    int missingNumIndex = 0;
+    int comparisonArrIndex = 0;
+
+    //puts n values in an array for comparison
+    for (int i = 0; i <= n; i++){
+        if (i != n){
+            comparisonArr[comparisonArrIndex] = i;
+            comparisonArrIndex++;
+        } if (i == n){
+            comparisonArr[comparisonArrIndex] = i;
+        }
+    }
+
+    //print out original array
+    cout << "Original array: ";
+    for (int i = 0; i < n; i++){
+        cout << arr[i] << ' ';
+    }
+
+    cout << "\nOriginal array nums size: " << comparisonArrIndex;
+
+    cout << "\nFind missing values results " << endl;
+
+    for (int i = 0; i <= n; i++){
+        for (int j = 0; j < n; j++){
+            
+            if (comparisonArr[i] == arr[j]){
+                numIsPresent = true;
+                cout << comparisonArr[i] << " is TRUE at value " << arr[j] << endl;
+                cout << "--" << endl;
+                break;
+            } else if (comparisonArr[i] != arr[j] && (j < n-1)){ // checks for missing values before the end of the possible options
+                //adds missing values to the new array
+                numIsPresent = false;
+                cout << comparisonArr[i] << " is FALSE at value " << arr[j] << endl;
+            } else if (missingNumsArr[i] != arr[j] && (j = n-1)){ // checks for missing values at the end of the possible options
+                numIsPresent = false;
+                cout << comparisonArr[i] << " is FALSE at value " << arr[j] << endl;
+                missingNumsArr[missingNumIndex] = comparisonArr[i];
+                cout << "Updated Missing Numbers Array at index " << missingNumIndex << " = " << missingNumsArr[missingNumIndex] << endl;
+                missingNumIndex++;
+                cout << "--" << endl;
+            }
+        }
+    }
+
+    cout << "Missing num array size: " << missingNumIndex; //prints amount of numbers in missing num array;
+
+    cout << "\nMissing num array: ";
+    //prints numbers in missing num array with brackets on each side of array
+    for (int i = 0; i < missingNumIndex; i++){
+      if (i == 0){
+        cout << "[" << missingNumsArr[i] << ", ";
+      } else if (i > 0 && i < missingNumIndex-1){
+        cout << missingNumsArr[i] << ", ";
+      } else if (i > 0 && i == missingNumIndex-1) {
+        cout << missingNumsArr[i] << "]" << endl; // don't print comma on last array element
+      }
+    }
+
+    return missingNumsArr;
+    delete[] comparisonArr;
+    delete[] missingNumsArr;
+}
