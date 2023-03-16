8c078912ace5177f86c9f2861a4d486da9237626
Jovanay Carter <69544825+jovanayc@users.noreply.github.com>
2023-03-15 23:25:02 -0400

    added more comments to hw10

--- tandon_HW10/N11316951_hw10_q1.cpp
+++ tandon_HW10/N11316951_hw10_q1.cpp
@@ -13,8 +13,11 @@ using namespace std;
 string* createWordsArray(string sentence, int& outWordsArrSize);
 
 int main(){  
-  string sentence = "Lions and tigers and bears Oh my.";
+  string sentence;
   int outWordsArrSize = 0;
+
+  cout << "Please enter your sentence:" << endl;
+  getline (cin, sentence);
   
   createWordsArray (sentence, outWordsArrSize);
   
@@ -26,7 +29,6 @@ string* createWordsArray(string sentence, int& outWordsArrSize){
     int delimiterIndex = 0;
     int previousDelimiterIndex = 0;
     string newElementWord;
-    //vector <string> newStringArr;
 
     string* createWordsArray;
     createWordsArray = new string [outWordsArrSize];
@@ -35,28 +37,26 @@ string* createWordsArray(string sentence, int& outWordsArrSize){
     //find space delimiter
     for (int i = 0; i < sentence.length()-1; i++){
       
-        //find instances of delimiters (space, commas, or periods)
-        if (((sentence[i] >= 'a' && sentence[i] <= 'z') || (sentence[i] >= 'A' && sentence[i] <= 'Z'))
-          && (sentence[i+1] == ' ' || sentence[i+1] == '.' || sentence[i+1] == ',')){
-          delimiterIndex = (i+1) - previousDelimiterIndex;
-          cout << delimiterIndex << endl; //expected 5, 9
-  
-          //once you find delimiter, updated the substr
-          newElementWord = sentence.substr(previousDelimiterIndex, delimiterIndex);
-          cout << newElementWord << endl;
-  
-          //add new substr into array of text
-          createWordsArray[wordsArrIndex] = newElementWord; //updated words array with current word
-          wordsArrIndex ++; // increate index for words arr
+      //find instances of delimiters (space, commas, or periods)
+      if (((sentence[i] >= 'a' && sentence[i] <= 'z') || (sentence[i] >= 'A' && sentence[i] <= 'Z'))
+        && (sentence[i+1] == ' ' || sentence[i+1] == ',' || sentence[i+1] == '.')){
+       
+        //set index of delimiter, resetting from 0 at the last delimiter each time by subtracting
+        delimiterIndex = (i+1) - previousDelimiterIndex;
+          //cout << delimiterIndex << endl; // developer check
 
-          //newStringArr.push_back(newElementWord);
-          outWordsArrSize ++;
-          
-          //update values
-          previousDelimiterIndex += delimiterIndex + 1;
-          //i++
-          
-        }
+        //once you find delimiter, updated the substr with index by starting at the last delimiter +1 and ending at the new delimiter.
+        newElementWord = sentence.substr(previousDelimiterIndex, delimiterIndex);
+          //cout << newElementWord << endl; // developer check
+
+        //add new substr into array of text
+        createWordsArray[wordsArrIndex] = newElementWord; //updated words array with current word
+        
+        //Update values for next iteration
+        wordsArrIndex ++; // increase index for words arr
+        outWordsArrSize ++;// increase size count of words arr
+        previousDelimiterIndex += delimiterIndex + 1; //reset previous delimiter          
+      }
     }
 
     //print out array in individual elements
@@ -69,7 +69,7 @@ string* createWordsArray(string sentence, int& outWordsArrSize){
       }
     }
   
-    cout << "\noutWordsArrSize: " << outWordsArrSize << endl;
+    cout << "\n Number of words in sentence / size of words in the new words array: " << outWordsArrSize << endl;
 
     return createWordsArray;
 
