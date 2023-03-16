f14f922514daac55457f390366da0bf447d8cfec
Jovanay Carter <69544825+jovanayc@users.noreply.github.com>
2023-03-16 16:35:13 -0400

    changes to hw10 q2 and added hw9 q3

---
+++ tandon_HW10/N11316951_hw10_q3.cpp
@@ -0,0 +1,14 @@
+//Jovanay Carter, N11316951, Winter Extended Bridge 2023
+//Homework 10, Question 3, March 17, 2023
+
+/* Prompt: Write two versions of a program that reads a sequence of positive numbers. Then prints
+all line numbers in sequence entered by the user that contain num or say num does not show in sequence.
+    Version 1) no vector
+    Version 2) should use vector
+Note: Linear time, meaning theta(n)
+Note: Write two functions main1() and main2() and test these in main()
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
--- tandon_HW9/N11316951_hw9_q3.cpp
+++ tandon_HW9/N11316951_hw9_q3.cpp
@@ -1,5 +1,5 @@
 //Jovanay Carter, N11316951, Winter Extended Bridge 2023
-//Homework 9, Question 3, March 10, 2023
+//Homework 9, Question 4, March 10, 2023
 
 /* Prompt: Write a program that gets an array of integers & its size; create a new array with positive numbers only.
 Should contain 4 versions of the function.
@@ -15,6 +15,8 @@ int* getPosNums1(int* arr, int arrSize, int& outPosArrSize);
 int main(){
     int arrSize;
     int num;
+    vector <int> positiveNums;
+    int posNumsCount = 0;
 
     cout << "Please enter array size: ";
     cin >> arrSize;
@@ -25,14 +27,28 @@ int main(){
     int userNums [arrSize];
     for (int i = 0; i < arrSize; i++){
         cin >> num;
-        userNums[i] = num;
-        cout << userNums[i] << ' '; // print values
+        userNums[i] = num; // adds all user values into an array
+        //cout << userNums[i] << ' '; // print values
+
+        if (num % 2 == 0){
+            positiveNums.push_back(num); // adds positive user values into an array
+            posNumsCount ++;
+        }
     }
+
+    cout << "Positive Values: ";
+    for (int i=0; i<posNumsCount; i++){
+        cout << positiveNums[i] << ' ';
+    }
+
+    //call getPosNums function
+    getPosNums1(userNums, arrSize, posNumsCount);
 
     return 0;
 }
 
 int* getPosNums1(int* arr, int arrSize, int& outPosArrSize){
+    int posNumArr[arrSize];
 
-    
+    return posNumArr;
 }
