6ece5ffebf5a06ad62c6840a9d299f6b88815798
Jovanay Carter <69544825+jovanayc@users.noreply.github.com>
2023-03-21 14:23:08 -0400

    hw11q1 additiona

--- tandon_HW11/N11316951_hw11_q1.cpp
+++ tandon_HW11/N11316951_hw11_q1.cpp
@@ -2,30 +2,63 @@
 //Homework 11, Question 1, March 24, 2023
 
 /* Prompt: Write three recursive programs
-    1) Prints a right trangl
-    2) Prints hourglass with right triangles only
-    3) Prints ruler marks of 2^n - 1 marks
+1) Prints a right trangl
+2) Prints hourglass with right triangles only
+3) Prints ruler marks of 2^n - 1 marks
 */
 
 #include <iostream>
 using namespace std;
 
-void triangle (int n){
-    if (n==1){ // base case
-        cout << '*';
-    } else {
-        triangle (n-1);
-        for (int i = 0; i < n; i++){ // for recursive steps, print n number of stars
-            cout <<"*";
-        }
-    }
+void triangle (int n);
+void printOppositeTriangles(int n);
 
+int main() {
+    int n = 4; // hard code 4 for example
+
+    cout << "a) Triangle" << endl;
+    triangle(n);
+
+    cout << "\n// BREAK //\n" << endl;
+
+    cout << "b) Opposite Triangle" << endl;
+    printOppositeTriangles(n);
+    return 0;
+}
+
+void triangle (int n){
+    if (n==0){
+    return;
+    } else {
+    triangle (n-1);
+    for (int i = 0; i < n; i++){ // for recursive steps, print n number of stars
+    cout << '*';
+    }
+    }
     cout << endl; // go to next line at the end of each print
 }
 
-int main() {
-    int n = 4;
+void printOppositeTriangles(int n){
+    int max = n;
 
-    //Call triangle function
-    triangle(n);
-}
+    //base case for top & bottom
+    if (n==0){
+        return;
+
+    } else {
+        //Top half
+        for (int j = max; j > 0; j--){ // using max value so it does not affect recursion of n
+            cout << '*';
+        } 
+
+        cout << endl; // go to next line at the end of each print
+
+        //Bottom half
+        printOppositeTriangles (n-1);
+        for (int i = 0; i < n; i++){
+            cout << '*';
+        }
+    }
+
+    cout << endl; // changing lines at the end of every recursive line
+} 
