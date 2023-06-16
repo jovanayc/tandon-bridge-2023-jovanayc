d3821ba470636bc99a0c3d2f8d893d3808f04acf
Jovanay Carter <69544825+jovanayc@users.noreply.github.com>
2023-06-15 21:06:03 -0400

    q12.cpp not perfect

--- Exam4/clothing-q12.cpp
+++ Exam4/clothing-q12.cpp
@@ -1,20 +1,20 @@
 #include <iostream>
 using namespace std;
 
-class Person{ //Person that owns or trades clothes
-
-};
-
 class Clothing { //clothing worn by a Person
 private: 
     string articleName;
     string articleDescription;
+
 public:
     bool articleOwner;
     string ownerName;
+    Clothing* itemPtr; //in order to access item
 
+    //copy constructor
     Clothing (string newName, string newDescription) : articleName (newName), articleDescription (newDescription), articleOwner(false) {
         cout << "I'm a " << articleName << " and no one owns me yet :(" << endl;
+        itemPtr = nullptr;
     };
 
     //clothing methods
@@ -24,15 +24,58 @@ public:
     }
 
     bool isOwned () {
-        if (articleOwner){
-            cout << ownerName << endl;
-            return true;
-        } else 
+        if (!articleOwner){
             cout << "shirt is not owned" << endl;
-            return false;    
+            return false; 
+        } else 
+            cout << ownerName << endl;
+            return true; 
+    }
+
+    //deconstructor
+    ~Clothing();
+};
+
+class Person{ //Person that owns or trades clothes
+private: 
+    string personName;
+    int inventory;
+
+public:
+    //copy constructor
+    Person (string newName) : personName (newName) {
+        //set person name to jane doe if they do not enter a name or enter a blank space
+        if (newName == "" || newName == " "){
+            personName = "Jane Doe";
+        }
+
+        inventory = 0;
+    };
+
+    //methods
+    string getPersonName () {
+        //print and return personName
+        cout << personName << endl;
+        return personName;
+    };
+
+    void purchaseClothing (Clothing* name) {
+        if (name->itemPtr != nullptr){ //if the item is already assigned to another person
+            //reject purchase
+            cout << "Item is already owned. Cannot complete purchase." << endl;
+        } else {
+            name->itemPtr = personName;
+            inventory ++;
+            return true;
+        }  
+    }
+
+    void countInventory(){
+        cout << "Current Inventory count: " << inventory << endl;
     }
 
 };
+
 
 int main (){
     //test code for clothing class
@@ -43,5 +86,10 @@ int main (){
     cout << "Shirt Owner Name: " << Shirt.ownerName << endl;
     Shirt.isOwned();
     Shirt.getClothingDescription();
+
+    //test code for person class
+    Person Jovanay ("Jovanay");
+    Jovanay.getPersonName();
+    Jovanay.countInventory();
 
 }
